212954589a0075a1188c48974c79372a3dab4aaa
ARGL <92689016+alejandro026@users.noreply.github.com>
2023-12-08 16:34:57 -0600

    Configuracion

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)  # Esto habilitará CORS para todas las rutas
 @app.route('/')
 def home():
     return render_template('home.html')
