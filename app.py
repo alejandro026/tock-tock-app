6d0360d46d90766bd2326ad28a927597c3e74efc
ARGL <92689016+alejandro026@users.noreply.github.com>
2023-12-08 16:25:22 -0600

    Agregano aplicacion

--- app.py
+++ app.py
@@ -1,30 +1,10 @@
-from flask import Flask, jsonify, request
-from flask_pymongo import PyMongo
-from flask_cors import CORS
-from bson.json_util import dumps
-import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
-CORS(app)  # Esto habilitará CORS para todas las rutas
-app.config["MONGO_URI"] = "mongodb+srv://Johan:Johan@dbloginreact.2ljvu.mongodb.net/AventurasMexicanas"
-mongo = PyMongo(app)
 
-@app.route('/add_score', methods=['POST'])
-def add_score():
-    data = request.get_json()
-    print(data)
-    name = data['nombreJugador']
-    score = data['puntos']
-    mongo.db.nivel_dos.insert_one({'nombreJugador': name, 'puntos': score})
-    return jsonify(message="Score added successfully"), 201
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-@app.route('/top_scores', methods=['GET'])
-def get_top_scores():
-    top_scores = list(mongo.db.nivel_dos.find().sort('puntos', -1).limit(5))
-    for score in top_scores:
-        score['_id'] = str(score['_id'])  # Convertir ObjectId a string
-    return jsonify(top_scores), 200
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
